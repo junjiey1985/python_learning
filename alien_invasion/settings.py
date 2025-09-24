e56f845b0cbd398e2d10f5857268ba5a2011dca5
junjiey1985 <332932669@qq.com>
2025-09-24 19:46:09 +0800

    第13章 外星人

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -9,6 +9,7 @@ class Settings:
 
         # 飞船的设置
         self.ship_speed = 1.5
+        self.ship_limit = 3
 
         # 子弹设置
         self.bullet_speed = 2.0
@@ -16,3 +17,9 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
+
+        # 外星人设置
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction 为 1 表示向右移动，为-1 表示向左移动
+        self.fleet_direction = 1
