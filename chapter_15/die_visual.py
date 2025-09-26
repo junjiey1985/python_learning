e0975ce8d8e2ccd6125867b4acfafbe69f7148a1
junjiey1985 <332932669@qq.com>
2025-09-26 20:48:21 +0800

    第15章 生成数据并可视化

---
+++ chapter_15/die_visual.py
@@ -0,0 +1,27 @@
+import plotly.express as px
+
+from die import Die
+
+# 创建一个 D6
+die = Die()
+
+# 掷几次骰子并将结果存储在一个列表中
+results = []
+for roll_num in range(1000):
+    result = die.roll()
+    results.append(result)
+
+# 分析结果
+frequencies = []
+poss_results = range(1, die.num_sides+1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# 对结果进行可视化
+title = "Results of Rolling One D6 1,000 Times"
+labels = {'x': 'Result', 'y': 'Frequency of Result'}
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+fig.show()
+
+
